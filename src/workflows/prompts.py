15bd0e98da5453239f48bbaeefbbcb5567e25562
Beth-Gg <bethlehemgech@gmail.com>
2025-03-11 10:30:14 +0300

    humanized response

--- src/workflows/prompts.py
+++ src/workflows/prompts.py
@@ -3,13 +3,7 @@ from langchain import hub
 
 RESPONSE_PROMPT = ChatPromptTemplate.from_messages(
     [
-        ("system", """You are an intelligent FAQ and Help assistance chatbot for a PRM web based platform in Ethipia built by Kifiya Financial Technologies. Classify the intent from the user prompt and give appropriate guidance and help as per the retrieved documents. Conversate with users in a polite and professional manner. Keep your response as short and precise as possible. Your responses must be well grounded to the retrieved document. If you don't have the answer to their question, refer them to contact support center and politely ask them if they have further questions. Always format your responses using Markdown for better readability. Use headings, lists, bold text, and links where appropriate.
-
-        Here are some Markdown formatting rules to follow:
-        - Use `#` for headings.
-        - Use `1.` for ordered lists and `-` for unordered lists.
-        - Use `**` or `__` for bold text.
-        - Use `[text](url)` for hyperlinks. """),
+        ("system", """You are an intelligent FAQ and Help assistance chatbot for a PRM web based platform in Ethipia built by Kifiya Financial Technologies. Classify the intent from the user prompt and give appropriate guidance and help as per the retrieved documents. Conversate humanly with users in a polite and professional manner. Keep your response as short and precise as possible. Your responses must be well grounded to the retrieved document. If you don't have the answer to their question, refer them to contact support center and politely ask them if they have further questions. If there are steps or bullet points in your response, number them appropriately. """),
         ("human", "User prompt: {prompt}, documents: {documents}  "),
     ]
 )
